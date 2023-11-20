987c136c4d7c591cd95596000056db4c11e43f6e
Isabel Silva <silva.i@northeastern.edu>
2023-11-20 15:24:30 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import session from "express-session";
+
+import express from "express";
+import Hello from "./hello.js";
+import Lab5 from "./lab5.js";
+import CourseRoutes from "./Database/courses/routes.js";
+import ModuleRoutes from "./Database/modules/routes.js";
+
+import cors from "cors";
+const app = express();
+app.use(express.json());
+app.use(cors());
+ModuleRoutes(app);
+
+CourseRoutes(app);
+Hello(app);
+Lab5(app);
+
+app.listen(process.env.PORT || 4000);
+
