ef6de941d420ce5e17a270a7d68b8a719dbccb2c
Isabel Silva <silva.i@northeastern.edu>
2023-12-05 01:17:25 -0500

    assignment 6

--- app.js
+++ app.js
@@ -5,13 +5,39 @@ import Hello from "./hello.js";
 import Lab5 from "./lab5.js";
 import CourseRoutes from "./Database/courses/routes.js";
 import ModuleRoutes from "./Database/modules/routes.js";
+import UserRoutes from "./users/routes.js";
+import session from "express-session";
 
 import cors from "cors";
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb+srv://silvai:governador100@cluster0.mne8riy.mongodb.net/?retryWrites=true&w=majority" || "mongodb://localhost:27017/kanbas");
+
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+    cors({
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    })
+   );
+   const sessionOptions = {
+    secret: "any string",
+    resave: false,
+    saveUninitialized: false,
+  };
+  if (process.env.NODE_ENV !== "development") {
+    sessionOptions.proxy = true;
+    sessionOptions.cookie = {
+      sameSite: "none",
+      secure: true,
+    };
+  }
+  app.use(session(sessionOptions));
+  
+     
+UserRoutes(app);
 ModuleRoutes(app);
-
 CourseRoutes(app);
 Hello(app);
 Lab5(app);
